84ce9216827d289ca69fcdb33c7bb076ba4e7589
Arian <arian.kulmer@web.de>
2024-12-28 20:49:24 +0100

    sm8550-common: udfps: Compare against the aidl AcquiredInfo
    Change-Id: I5b6eb5a4544373d71b89abc0e301126eb55e31d1

--- udfps/UdfpsHandler.cpp
+++ udfps/UdfpsHandler.cpp
@@ -6,6 +6,7 @@
 
 #define LOG_TAG "UdfpsHandler.xiaomi_sm8550"
 
+#include <aidl/android/hardware/biometrics/fingerprint/BnFingerprint.h>
 #include <android-base/logging.h>
 #include <android-base/unique_fd.h>
 
@@ -36,6 +37,8 @@
 #define TOUCH_DEV_PATH "/dev/xiaomi-touch"
 
 #define FOD_PRESS_STATUS_PATH "/sys/class/touch/touch_dev/fod_press_status"
+
+using ::aidl::android::hardware::biometrics::fingerprint::AcquiredInfo;
 
 namespace {
 
@@ -208,7 +211,7 @@ class XiaomiSM8550UdfpsHander : public UdfpsHandler {
 
     void onAcquired(int32_t result, int32_t vendorCode) {
         LOG(DEBUG) << __func__ << " result: " << result << " vendorCode: " << vendorCode;
-        if (result == FINGERPRINT_ACQUIRED_GOOD) {
+        if (static_cast<AcquiredInfo>(result) == AcquiredInfo::GOOD) {
             setFingerDown(false);
         }
     }
