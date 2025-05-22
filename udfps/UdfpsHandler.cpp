1459b595d1f35b319eab591f79ee7fe9a14d0957
Arian <arian.kulmer@web.de>
2025-05-22 11:35:52 +0200

    sm8550-common: udfps: Disable lhbm when authentication is finished
    We previously missed the case when the finger is rejected the
    fifth time and neither cancel nor a successful acquired message
    was sent, leading to lhbm staying on forever. These new methods
    should cover all cases in which the authentication is over. We
    do not need to handle onAcquired and cancel explicitly anymore,
    so drop the corresponding methods.
    
    Change-Id: Ibc284bc250e60a46d220f6a8f4c82908cd1f0776

--- udfps/UdfpsHandler.cpp
+++ udfps/UdfpsHandler.cpp
@@ -1,12 +1,11 @@
 /*
- * Copyright (C) 2022 The LineageOS Project
+ * Copyright (C) 2022-2025 The LineageOS Project
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #define LOG_TAG "UdfpsHandler.xiaomi_sm8550"
 
-#include <aidl/android/hardware/biometrics/fingerprint/BnFingerprint.h>
 #include <android-base/logging.h>
 #include <android-base/unique_fd.h>
 
@@ -35,8 +34,6 @@
 
 #define DISP_FEATURE_PATH "/dev/mi_display/disp_feature"
 #define TOUCH_DEV_PATH "/dev/xiaomi-touch"
-
-using ::aidl::android::hardware::biometrics::fingerprint::AcquiredInfo;
 
 namespace {
 
@@ -180,32 +177,9 @@ class XiaomiSM8550UdfpsHandler : public UdfpsHandler {
         ioctl(touch_fd_.get(), TOUCH_IOC_SET_CUR_VALUE, &touchRequest);
     }
 
-    void onAcquired(int32_t result, int32_t vendorCode) {
-        LOG(DEBUG) << __func__ << " result: " << result << " vendorCode: " << vendorCode;
-        if (static_cast<AcquiredInfo>(result) == AcquiredInfo::GOOD) {
-            onFingerUp();
-        }
-    }
+    void onAuthenticationSucceeded() { onFingerUp(); }
 
-    void cancel() {
-        LOG(DEBUG) << __func__;
-        enrolling = false;
-    }
-
-    void preEnroll() {
-        LOG(DEBUG) << __func__;
-        enrolling = true;
-    }
-
-    void enroll() {
-        LOG(DEBUG) << __func__;
-        enrolling = true;
-    }
-
-    void postEnroll() {
-        LOG(DEBUG) << __func__;
-        enrolling = false;
-    }
+    void onAuthenticationFailed() { onFingerUp(); }
 
   private:
     fingerprint_device_t* mDevice;
