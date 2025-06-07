10c87cfa7e174fe4e81b934e7769df8dd7f55854
Bruno Martins <bgcngm@gmail.com>
2025-06-07 18:38:16 +0100

    sm8550-common: Switch to Touch AIDL
    Change-Id: Id4fcd7ca97b04f887dec6939419815722097d10c
    Signed-off-by: Lunark :3 <jvillaltamatos3007@gmail.com>

--- common.mk
+++ common.mk
@@ -352,6 +352,9 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal-service.qti
 
 # Touchscreen
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch-service.xiaomi
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
