3d5f005c7deffbe4098c4b60333c5e8d82afd47a
Lunark :3 <jvillaltamatos3007@gmail.com>
2025-11-21 13:13:28 -0500

    sm8550-common: udfps: Reset udfpshandler
    - This udfps handler was not working properly; it worked intermittently and did not work on some devices when the screen was set to 60Hz, so we reset it to the original commit.
    - HEAD: https://github.com/LineageOS/android_device_xiaomi_sm8450-common/commit/d9d3600817e41253983b455e438267d7befd0c3d
    
    This reverts commit cf885fe34bf410709088893c2e20594f23b679b6.
    
    Revert "sm8550-common: add fod_press_status path for udfps"
    
    This reverts commit 62383fcb92415ffed3d40cf02535cdca17592164.
    
    Revert "socrates: udfps: Update FINGERPRINT_ACQUIRED_VENDOR value"
    
    This reverts commit 6686a14ea73d8165b979f21ab48e8957ee89b791.
    
    Revert "socrates: udfps: Remove unused code"
    
    This reverts commit 6c5bedf0bda311e8fc540403dd54774988a312de.
    
    Revert "socrates: udfps: Handle failed onAcquired results"
    
    This reverts commit 49a93f5ab3c5c9d5101b2ddb26607980d469687e.
    
    Revert "socrates: udfps: Remove fod_press_status polling"
    
    This reverts commit dbb27df4f57c9bb46da83533a2830bddeca86710.
    
    Change-Id: Iab71cc5c07661e38ce8d7b9aa540fbbc4935720d

--- udfps/UdfpsHandler.cpp
+++ udfps/UdfpsHandler.cpp
@@ -1,14 +1,12 @@
 /*
- * Copyright (C) 2022-2025 The LineageOS Project
+ * Copyright (C) 2022 The LineageOS Project
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #define LOG_TAG "UdfpsHandler.xiaomi_sm8550"
 
-#include <aidl/android/hardware/biometrics/fingerprint/BnFingerprint.h>
 #include <android-base/logging.h>
-#include <android-base/properties.h>
 #include <android-base/unique_fd.h>
 
 #include <poll.h>
@@ -16,208 +14,147 @@
 #include <fstream>
 #include <thread>
 
-#include <display/drm/mi_disp.h>
-#include <linux/xiaomi_touch.h>
-
 #include "UdfpsHandler.h"
+#include "xiaomi_touch.h"
 
 #define COMMAND_NIT 10
-#define TARGET_BRIGHTNESS_OFF 0
-#define TARGET_BRIGHTNESS_1000NIT 1
-#define TARGET_BRIGHTNESS_110NIT 6
-
-#define LOW_BRIGHTNESS_THRESHHOLD 100
+#define PARAM_NIT_FOD 1
+#define PARAM_NIT_NONE 0
 
 #define COMMAND_FOD_PRESS_STATUS 1
-#define COMMAND_FOD_PRESS_X 2
-#define COMMAND_FOD_PRESS_Y 3
 #define PARAM_FOD_PRESSED 1
 #define PARAM_FOD_RELEASED 0
 
-#define DISP_FEATURE_PATH "/dev/mi_display/disp_feature"
-#define TOUCH_DEV_PATH "/dev/xiaomi-touch"
+#define FOD_STATUS_OFF 0
+#define FOD_STATUS_ON 1
 
-using ::aidl::android::hardware::biometrics::fingerprint::AcquiredInfo;
+#define TOUCH_DEV_PATH "/dev/xiaomi-touch"
+#define TOUCH_ID 0
+#define TOUCH_MAGIC 'T'
+#define TOUCH_IOC_SET_CUR_VALUE _IO(TOUCH_MAGIC, SET_CUR_VALUE)
+#define TOUCH_IOC_GET_CUR_VALUE _IO(TOUCH_MAGIC, GET_CUR_VALUE)
+
+#define DISP_PARAM_PATH "sys/devices/virtual/mi_display/disp_feature/disp-DSI-0/disp_param"
+#define DISP_PARAM_LOCAL_HBM_MODE "9"
+#define DISP_PARAM_LOCAL_HBM_OFF "0"
+#define DISP_PARAM_LOCAL_HBM_ON "1"
+
+#define FOD_PRESS_STATUS_PATH "/sys/class/touch/touch_dev/fod_press_status"
 
 namespace {
 
-static std::shared_ptr<disp_event_resp> parseDispEvent(int fd) {
-    disp_event header;
-    ssize_t headerSize = read(fd, &header, sizeof(header));
-    if (headerSize < sizeof(header)) {
-        LOG(ERROR) << "unexpected display event header size: " << headerSize;
-        return nullptr;
-    }
-
-    std::shared_ptr<disp_event_resp> response(static_cast<disp_event_resp*>(malloc(header.length)),
-                                              free);
-    if (!response) {
-        LOG(ERROR) << "failed to allocate memory for display event response";
-        return nullptr;
-    }
-    response->base = header;
-
-    int dataLength = response->base.length - sizeof(response->base);
-    if (dataLength < 0) {
-        LOG(ERROR) << "invalid data length: " << response->base.length;
-        return nullptr;
-    }
-
-    ssize_t dataSize = read(fd, &response->data, dataLength);
-    if (dataSize < dataLength) {
-        LOG(ERROR) << "unexpected display event data size: " << dataSize;
-        return nullptr;
-    }
-
-    return response;
+template <typename T>
+static void set(const std::string& path, const T& value) {
+    std::ofstream file(path);
+    file << value;
 }
 
-struct disp_base displayBasePrimary = {
-        .flag = 0,
-        .disp_id = MI_DISP_PRIMARY,
-};
+static bool readBool(int fd) {
+    char c;
+    int rc;
+
+    rc = lseek(fd, 0, SEEK_SET);
+    if (rc) {
+        LOG(ERROR) << "failed to seek fd, err: " << rc;
+        return false;
+    }
+
+    rc = read(fd, &c, sizeof(char));
+    if (rc != 1) {
+        LOG(ERROR) << "failed to read bool from fd, err: " << rc;
+        return false;
+    }
+
+    return c != '0';
+}
 
 }  // anonymous namespace
 
-class XiaomiSm8550UdfpsHandler : public UdfpsHandler {
+class XiaomiSM8550UdfpsHander : public UdfpsHandler {
   public:
     void init(fingerprint_device_t* device) {
         mDevice = device;
-        disp_fd_ = android::base::unique_fd(open(DISP_FEATURE_PATH, O_RDWR));
         touch_fd_ = android::base::unique_fd(open(TOUCH_DEV_PATH, O_RDWR));
 
-        // Thread to listen for fod ui changes
         std::thread([this]() {
-            android::base::unique_fd fd(open(DISP_FEATURE_PATH, O_RDWR));
+            int fd = open(FOD_PRESS_STATUS_PATH, O_RDONLY);
             if (fd < 0) {
-                LOG(ERROR) << "failed to open " << DISP_FEATURE_PATH << " , err: " << fd;
+                LOG(ERROR) << "failed to open fd, err: " << fd;
                 return;
             }
 
-            // Register for FOD events
-            struct disp_event_req displayEventRequest = {
-                    .base = displayBasePrimary,
-                    .type = MI_DISP_EVENT_FOD,
-            };
-            if (ioctl(fd.get(), MI_DISP_IOCTL_REGISTER_EVENT, &displayEventRequest) < 0) {
-                LOG(ERROR) << "failed to register FOD event";
-                return;
-            }
-
-            struct pollfd dispEventPoll = {
-                    .fd = fd.get(),
-                    .events = POLLIN,
+            struct pollfd fodPressStatusPoll = {
+                    .fd = fd,
+                    .events = POLLERR | POLLPRI,
                     .revents = 0,
             };
 
             while (true) {
-                int rc = poll(&dispEventPoll, 1, -1);
+                int rc = poll(&fodPressStatusPoll, 1, -1);
                 if (rc < 0) {
-                    LOG(ERROR) << "failed to poll " << DISP_FEATURE_PATH << ", err: " << rc;
+                    LOG(ERROR) << "failed to poll fd, err: " << rc;
                     continue;
                 }
 
-                std::shared_ptr<disp_event_resp> response = parseDispEvent(fd.get());
-                if (!response) {
-                    continue;
-                }
-
-                if (response->base.type != MI_DISP_EVENT_FOD) {
-                    LOG(ERROR) << "unexpected display event: " << response->base.type;
-                    continue;
-                }
-
-                int value = response->data[0];
-                LOG(DEBUG) << "received data: " << std::bitset<8>(value);
-
-                bool localHbmUiReady = value & LOCAL_HBM_UI_READY;
-                bool requestLowBrightnessCapture = value & FOD_LOW_BRIGHTNESS_CAPTURE;
-
-                mDevice->extCmd(mDevice, COMMAND_NIT,
-                                localHbmUiReady
-                                        ? (requestLowBrightnessCapture ? TARGET_BRIGHTNESS_110NIT
-                                                                       : TARGET_BRIGHTNESS_1000NIT)
-                                        : TARGET_BRIGHTNESS_OFF);
+                mDevice->extCmd(mDevice, COMMAND_FOD_PRESS_STATUS,
+                                readBool(fd) ? PARAM_FOD_PRESSED : PARAM_FOD_RELEASED);
             }
         }).detach();
     }
 
-    void onFingerDown(uint32_t x, uint32_t y, float /*minor*/, float /*major*/) {
-        LOG(DEBUG) << __func__ << "x: " << x << ", y: " << y;
-
-        mDevice->extCmd(mDevice, COMMAND_FOD_PRESS_X, x);
-        mDevice->extCmd(mDevice, COMMAND_FOD_PRESS_Y, y);
-        mDevice->extCmd(mDevice, COMMAND_FOD_PRESS_STATUS, PARAM_FOD_PRESSED);
-
-        // Update fod_finger_state node in case hwmodule polls it
-        struct touch_mode_request touchRequest = {
-                .mode = TOUCH_MODE_FOD_FINGER_STATE,
-                .value = 1,
-        };
-        ioctl(touch_fd_.get(), TOUCH_IOC_SET_CUR_VALUE, &touchRequest);
-
-        // Request HBM
-        struct disp_local_hbm_req displayLhbmRequest = {
-                .base = displayBasePrimary,
-                .local_hbm_value = LHBM_TARGET_BRIGHTNESS_WHITE_1000NIT,
-        };
-        ioctl(disp_fd_.get(), MI_DISP_IOCTL_SET_LOCAL_HBM, &displayLhbmRequest);
+    void onFingerDown(uint32_t /*x*/, uint32_t /*y*/, float /*minor*/, float /*major*/) {
+        LOG(INFO) << __func__;
+        setFingerDown(true);
     }
 
     void onFingerUp() {
-        LOG(DEBUG) << __func__;
-
-        mDevice->extCmd(mDevice, COMMAND_FOD_PRESS_X, 0);
-        mDevice->extCmd(mDevice, COMMAND_FOD_PRESS_Y, 0);
-        mDevice->extCmd(mDevice, COMMAND_FOD_PRESS_STATUS, PARAM_FOD_RELEASED);
-
-        // Disable HBM
-        struct disp_local_hbm_req displayLhbmRequest = {
-                .base = displayBasePrimary,
-                .local_hbm_value = LHBM_TARGET_BRIGHTNESS_OFF_FINGER_UP,
-        };
-        ioctl(disp_fd_.get(), MI_DISP_IOCTL_SET_LOCAL_HBM, &displayLhbmRequest);
-
-        // Update fod_finger_state node in case hwmodule polls it
-        struct touch_mode_request touchRequest = {
-                .mode = TOUCH_MODE_FOD_FINGER_STATE,
-                .value = 0,
-        };
-        ioctl(touch_fd_.get(), TOUCH_IOC_SET_CUR_VALUE, &touchRequest);
+        LOG(INFO) << __func__;
+        setFingerDown(false);
     }
 
     void onAcquired(int32_t result, int32_t vendorCode) {
-        LOG(DEBUG) << __func__ << " result: " << result << " vendorCode: " << vendorCode;
-        switch (static_cast<AcquiredInfo>(result)) {
-            case AcquiredInfo::GOOD:
-            case AcquiredInfo::PARTIAL:
-            case AcquiredInfo::INSUFFICIENT:
-            case AcquiredInfo::SENSOR_DIRTY:
-            case AcquiredInfo::TOO_SLOW:
-            case AcquiredInfo::TOO_FAST:
-            case AcquiredInfo::TOO_DARK:
-            case AcquiredInfo::TOO_BRIGHT:
-            case AcquiredInfo::IMMOBILE:
-            case AcquiredInfo::LIFT_TOO_SOON:
-                onFingerUp();
-                break;
-            default:
-                break;
+        LOG(INFO) << __func__ << " result: " << result << " vendorCode: " << vendorCode;
+        if (result == FINGERPRINT_ACQUIRED_GOOD) {
+            setFingerDown(false);
+            setFodStatus(FOD_STATUS_OFF);
+        } else if (vendorCode == 21 || vendorCode == 23) {
+            /*
+             * vendorCode = 21 waiting for fingerprint authentication
+             * vendorCode = 23 waiting for fingerprint enroll
+             */
+            setFodStatus(FOD_STATUS_ON);
         }
     }
 
-    void onAuthenticationSucceeded() { onFingerUp(); }
-
-    void onAuthenticationFailed() { onFingerUp(); }
+    void cancel() {
+        LOG(INFO) << __func__;
+        setFingerDown(false);
+        setFodStatus(FOD_STATUS_OFF);
+    }
 
   private:
     fingerprint_device_t* mDevice;
-    android::base::unique_fd disp_fd_;
     android::base::unique_fd touch_fd_;
+
+    void setFodStatus(int value) {
+        int buf[MAX_BUF_SIZE] = {TOUCH_ID, Touch_Fod_Enable, value};
+        ioctl(touch_fd_.get(), TOUCH_IOC_SET_CUR_VALUE, &buf);
+    }
+
+    void setFingerDown(bool pressed) {
+        mDevice->extCmd(mDevice, COMMAND_NIT, pressed ? PARAM_NIT_FOD : PARAM_NIT_NONE);
+
+        int buf[MAX_BUF_SIZE] = {TOUCH_ID, THP_FOD_DOWNUP_CTL, pressed ? 1 : 0};
+        ioctl(touch_fd_.get(), TOUCH_IOC_SET_CUR_VALUE, &buf);
+
+        set(DISP_PARAM_PATH,
+            std::string(DISP_PARAM_LOCAL_HBM_MODE) + " " +
+                    (pressed ? DISP_PARAM_LOCAL_HBM_ON : DISP_PARAM_LOCAL_HBM_OFF));
+    }
 };
 
 static UdfpsHandler* create() {
-    return new XiaomiSm8550UdfpsHandler();
+    return new XiaomiSM8550UdfpsHander();
 }
 
 static void destroy(UdfpsHandler* handler) {
