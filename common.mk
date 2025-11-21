7c7cfaf84cfbc3002e87c6ed983d64750caa5282
TheMysticle <36317711+TheMysticle@users.noreply.github.com>
2025-11-21 09:51:07 +0100

    sm8550-common: add sensor-notifier
    With the last kernel-modules commit we can finally add vendor.sensor-notifier for AOD Low Power Mode from AODNotifier.
    
    Kanged from: https://github.com/xiaomi-8550/android_device_xiaomi_sm8550-common/tree/beryl/sensors
    
    Test: Put phone into AOD, shine torch at it and confirm phone switches from LP1 to LP2 vice versa. Confirm screen goes blank on AOD when proximity sensor is covered without turning on pocket mode. Confirm screen no longer goes black when unlocking with always on fingerprint or double tap to wake.

--- common.mk
+++ common.mk
@@ -283,6 +283,7 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.xiaomi-multihal \
+    sensor-notifier \
     sensors.dynamic_sensor_hal
 
 PRODUCT_COPY_FILES += \
