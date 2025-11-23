b30f682801979cf4da029559aafcff1d95bda965
xt0032rus <andrey.denikin9@mail.com>
2025-11-23 22:27:31 +0300

    sm8550-common: Update Charging control for Lineage Health

--- common.mk
+++ common.mk
@@ -195,11 +195,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
-# Lineage Health
-$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
-$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
+# Charging control for Lineage Health HAL
 $(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/qcom-battery/night_charging)
-$(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
+$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
+$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
 
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
