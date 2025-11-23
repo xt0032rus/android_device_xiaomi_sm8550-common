08a13cdd18dae30e7554538745acd1574dcb1653
xt0032rus <andrey.denikin9@mail.com>
2025-11-23 22:30:01 +0300

    sm8550-common: Add XiaomiDolby

--- common.mk
+++ common.mk
@@ -26,6 +26,10 @@ PRODUCT_PACKAGES += \
     otapreopt_script \
     checkpoint_gc
 
+# Dolby Audio
+PRODUCT_PACKAGES += \
+    XiaomiDolby
+    
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.1-impl \
