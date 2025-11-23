f3c618d9abebedaebb87160f685beb8219a2eafd
xt0032rus <andrey.denikin9@mail.com>
2025-11-24 01:02:25 +0300

    sm8550-common: Add XiaomiParts

--- common.mk
+++ common.mk
@@ -26,10 +26,10 @@ PRODUCT_PACKAGES += \
     otapreopt_script \
     checkpoint_gc
 
-# Dolby Audio
+# XiaomiParts
 PRODUCT_PACKAGES += \
-    XiaomiDolby
-    
+    XiaomiParts
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.1-impl \
