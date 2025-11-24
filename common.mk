3ad8d47cfe4e78c1b80e5396c3de3b4f6279d002
xt0032rus <andrey.denikin9@mail.com>
2025-11-24 10:01:01 +0300

    Revert "sm8550-common: Add XiaomiParts"
    This reverts commit f3c618d9abebedaebb87160f685beb8219a2eafd.

--- common.mk
+++ common.mk
@@ -26,10 +26,10 @@ PRODUCT_PACKAGES += \
     otapreopt_script \
     checkpoint_gc
 
-# XiaomiParts
+# Dolby Audio
 PRODUCT_PACKAGES += \
-    XiaomiParts
-
+    XiaomiDolby
+    
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.1-impl \
