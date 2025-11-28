5fdadfd1d054e1dcf6b46b7933c1e02e20d98b91
lolipuru <banstellazz@gmail.com>
2025-11-28 17:25:40 +0800

    sm8550-common: Build sensors.xiaomi.v2
    Change-Id: I5de5a81b958d4af81429c802dd06591a602ec892
    Signed-off-by: lollipuru <banstellazz@gmail.com>
    Signed-off-by: WenHao2130 <wenhao2130@outlook.com>

--- common.mk
+++ common.mk
@@ -294,7 +294,11 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.xiaomi-multihal \
-    sensors.dynamic_sensor_hal
+    sensors.dynamic_sensor_hal \
+    sensors.xiaomi.v2
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/hals.conf:$(TARGET_COPY_OUT_ODM)/etc/sensors/hals.conf
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.dynamic.head_tracker.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/sku_kalama/android.hardware.sensor.dynamic.head_tracker.xml \
