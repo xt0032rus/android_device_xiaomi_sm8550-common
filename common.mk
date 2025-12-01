eca179024e1a6ce450f1b4f4d2bd6cbcd670e76a
TheMysticle <36317711+TheMysticle@users.noreply.github.com>
2025-12-01 01:36:55 +0100

    sm8550-common: introduce sensor-notifier for nonui handling
    Kanged from: https://github.com/xiaomi-8550/android_device_xiaomi_sm8550-common/tree/beryl/sensors and modified to work with our device configuration.
    
    Xiaomi.sensor.nonui only reports changes when the phone is covered AND vertical. It's a fusion sensor it uses proximity and Accelerometer. If you want to test it or log it do not just cover it, it won't do anything. You have to cover it and hold your phone vertically (upisde down is best), ask me how I know.....
    
    For now, no AODNotifier. I have it working, but it bugs out udfps and sometimes causes the display to go black.

--- common.mk
+++ common.mk
@@ -295,6 +295,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.xiaomi-multihal \
     sensors.dynamic_sensor_hal \
+    sensor-notifier \
     sensors.xiaomi.v2
 
 PRODUCT_COPY_FILES += \
